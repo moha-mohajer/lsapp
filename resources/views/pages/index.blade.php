e471f0ac679ec21d2981a4c217073c31f9761717
Moha-Mohajer <mohajeranii@hotmail.com>
2019-04-12 21:05:14 +0300

    end of video 3

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{config('app.name', 'LSAPP')}}</title>
+    </head>
+    <body>
+      <h1>Wellcome To Laravel</h1>
+      <p>This is first app created by Moha in Laravel</p>
+    </body>
+</html>
---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{config('app.name', 'LSAPP')}}</title>
+    </head>
+    <body>
+      <h1>Servies</h1>
+      <p>This is Servic page</p>
+    </body>
+</html>
